26e8a818c4832e87c02603696359e2e3f5a078df
ahhzaky <siahmadzaky@gmail.com>
2020-10-06 20:30:39 +0700

    chat_header

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>i am chat</h1>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
