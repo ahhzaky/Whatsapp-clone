bef3df44438afb1451bfd57d626048496ee2a72b
ahhzaky <siahmadzaky@gmail.com>
2020-10-06 21:20:50 +0700

    chat__footer and done frontend

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -3,6 +3,8 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 import React from "react";
 
@@ -47,6 +49,14 @@ function Chat() {
           This is message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
